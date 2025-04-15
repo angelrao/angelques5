74813bb64379eaa017bba48d9be2863df4bd8c99
angelrao <angel.rao.23cse@bmu.edu.in>
2025-04-15 20:59:30 +0530

    done all files

---
+++ app/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.application")
+
+
+}
+
+android {
+    namespace = "com.example.sol5"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.sol5"
+        minSdk = 21
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+
+    implementation("androidx.recyclerview:recyclerview:1.3.1")
+}
+
